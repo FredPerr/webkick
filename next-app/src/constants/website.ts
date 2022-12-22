8f1e2abc3677db5f077c2d05e4cdb7b01467644e
Frédéric Perron <frederic.norrep@gmail.com>
2022-12-21 21:09:06 -0500

    added website info base

---
+++ next-app/src/constants/website.ts
@@ -0,0 +1,6 @@
+export default {
+    title: 'Next.js Boilerplate',
+    keywords: 'next.js, boilerplate, typescript, react',
+    description: 'Next.js Boilerplate with TypeScript, and React.',
+    lang: 'en',
+}
