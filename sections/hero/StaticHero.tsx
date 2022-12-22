5f24473661ed331ec3b83f53abd5e5a0ab8946d3
Frédéric Perron <frederic.norrep@gmail.com>
2022-12-22 17:53:19 -0500

    made the hero section fit screen height

--- sections/hero/StaticHero.tsx
+++ sections/hero/StaticHero.tsx
@@ -3,7 +3,7 @@ import { Text } from "@nextui-org/react";
 
 export default function StaticHero(){
     return (
-        <Box id="hero" css={{h: '100vh', w: '100vw', bgColor: '$gray500', dflex: 'center', border: '5px solid $gray700'}}>
+        <Box id="hero" css={{d: 'flex', flex: '1 1 auto', w: '100%', bgColor: '$gray500', dflex: 'center', border: '5px solid $gray700', fd: 'column'}}>
             <Text css={{fontSize: '$4xl', color: '$gray700'}}>Hero background image</Text>
         </Box>
     )
