8102305fd4508cc9f40d7c2c0c3fd61f463c6c0f
Frédéric Perron <frederic.norrep@gmail.com>
2022-12-21 21:08:52 -0500

    added base routing paths and names

---
+++ next-app/src/constants/routing.ts
@@ -0,0 +1,14 @@
+export default {
+    HOME: {
+        path: '/',
+        title: 'Home'
+    },
+    ABOUT: {
+        path: '/about',
+        title: 'About'
+    },
+    CONTACT: {
+        path: '/contact',
+        title: 'Contact'
+    }
+}
