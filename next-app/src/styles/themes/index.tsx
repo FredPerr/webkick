ed12a18504ed669e0384e8ff51e17735adbf8dc0
Frédéric Perron <frederic.norrep@gmail.com>
2022-12-21 21:34:39 -0500

    added the nextui default theme

---
+++ next-app/src/styles/themes/index.tsx
@@ -0,0 +1,8 @@
+import { createTheme } from "@nextui-org/react";
+
+export default createTheme({
+    type: 'light',
+    theme: {
+        // Override theme attributes (for more information see https://nextui.org/docs/theme/customize-theme).
+    }
+})
