54125e37ec92a5774f5b704e92964c851f642a77
Frédéric Perron <frederic.norrep@gmail.com>
2022-12-21 21:00:43 -0500

    added configuration for SVGR

--- next-app/next.config.js
+++ next-app/next.config.js
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      resourceQuery: { not: [/url/] }, // exclude react component if *.svg?url
+      use: ["@svgr/webpack"]
+    },
+      {
+        test: /\.svg$/i,
+        type: 'asset',
+        resourceQuery: /url/,
+
+      })
+      return config
+    }
 }
 
 module.exports = nextConfig
