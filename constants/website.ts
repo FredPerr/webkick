55ae417ea477c184aab3616c0e16b63cd5677726
Frédéric Perron <frederic.norrep@gmail.com>
2022-12-22 17:51:58 -0500

    added the logo path in website constants

--- constants/website.ts
+++ constants/website.ts
@@ -1,6 +1,9 @@
+import AppLogo from "@/public/images/logo.svg?url"
+
 export default {
     title: 'Archetype - Next.js Boilerplate',
     keywords: 'next.js, boilerplate, typescript, react',
     description: 'Next.js Boilerplate with TypeScript, and React.',
     lang: 'en',
+    logo: AppLogo
 }
