2ce029d3bcecaf9c10ed9ab24f0b401e1e38932b
Frédéric Perron <frederic.norrep@gmail.com>
2022-12-23 01:41:57 -0500

    added the title for section component

---
+++ components/section/Title.tsx
@@ -0,0 +1,17 @@
+import { Text } from "@nextui-org/react";
+import Box from "../box";
+
+interface TitleProps {
+    title?: string,
+    subtitle?: string,
+}
+
+export default function Title(props: TitleProps) {
+    return (
+        <Box css={{my: 50}}>
+            {props.title && <Text h2 b css={{fontFamily: '12px'}}>{props.title}</Text>}
+            {props.subtitle && <Text h4 b>{props.subtitle}</Text>}
+        </Box>
+    )
+}
+
