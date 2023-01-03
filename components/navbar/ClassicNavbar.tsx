da4844870e5f736d6b53b337c174f9517ed30af7
Frédéric Perron <frederic.norrep@gmail.com>
2023-01-03 13:48:56 -0500

    added logo to navbar

--- components/navbar/ClassicNavbar.tsx
+++ components/navbar/ClassicNavbar.tsx
@@ -1,5 +1,12 @@
 import { Navbar } from '@nextui-org/react'
+import { LogoAnimated } from '../logo'
 
 export default function ClassicNavbar() {
-    return <Navbar variant="static"></Navbar>
+    return (
+        <Navbar variant="static">
+            <Navbar.Brand>
+                <LogoAnimated size={50}/>
+            </Navbar.Brand>
+        </Navbar>
+    )
 }
