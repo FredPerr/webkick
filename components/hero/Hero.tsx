13ac5d224a414d4e2163f3cca3367953905e75c8
Frédéric Perron <frederic.norrep@gmail.com>
2023-01-06 14:33:10 -0500

    added the hero img with filter

--- components/hero/Hero.tsx
+++ components/hero/Hero.tsx
@@ -16,7 +16,9 @@ export default function Hero() {
                     'radial-gradient(#4d4e50 0.45px, transparent 0.45px), radial-gradient(#4d4e50 0.45px, #262626 0.45px)',
                 backgroundSize: '20px 20px',
                 backgroundPosition: '0 0, 10px 10px',
-                // background: 'radial-gradient(22.71% 28.86% at 73.99% 40.17%, rgba(255, 108, 1, 0.09) 0%, rgba(255, 108, 1, 0) 100%), radial-gradient(19.26% 56.07% at 71.84% 40.17%, rgba(142, 39, 159, 0.207) 0%, rgba(226, 14, 14, 0) 100%), radial-gradient(32.09% 43.78% at 72.01% 46.78%, rgba(110, 167, 38, 0.2) 0%, rgba(142, 31, 182, 0) 100%)'
+                '@smMax': {
+                    h: '200vh',
+                },
             }}
         >
             <Box
@@ -28,8 +30,10 @@ export default function Hero() {
                     px: '5%',
                     ai: 'center',
                     ta: 'center',
+                    // background:
+                    // 'radial-gradient(19.26% 56.07% at 75% 75%, rgba(142, 39, 159, 0.1) 0%, rgba(226, 14, 14, 0) 100%), radial-gradient(32.09% 43.78% at 72.01% 46.78%, rgba(220, 132, 235, 0.03) 0%, rgba(142, 31, 182, 0) 100%)',
                     '@smMax': {
-                        gridTemplate: '50% 50% / 100%',
+                        gridTemplate: '1fr / 100%',
                     },
                 }}
             >
@@ -85,7 +89,14 @@ export default function Hero() {
                         <Button auto>En savoir plus</Button>
                     </Box>
                 </Box>
-                <Box css={{ p: '100px' }}>
+                <Box
+                    css={{
+                        d: 'flex',
+                        jc: 'center',
+                        h: '100%',
+                        filter: 'drop-shadow(0px 0px 30px rgba(200,200,255,0.1))',
+                    }}
+                >
                     <HeroImage />
                 </Box>
             </Box>
--- components/hero/HeroImage.tsx
+++ components/hero/HeroImage.tsx
@@ -1,5 +1,5 @@
 import HeroSvg from '@/public/images/hero/hero-img.svg'
 
 export default function HeroImage() {
-    return <HeroSvg style={{ width: 655, height: 685 }} />
+    return <HeroSvg />
 }
