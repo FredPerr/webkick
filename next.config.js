c3ff27061cc5518c40b762e486c238c0534fb9f2
Frédéric Perron <frederic.norrep@gmail.com>
2023-01-10 03:30:41 -0500

    added svgr

--- next.config.js
+++ next.config.js
@@ -1,6 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
+  trailingSlash: false,
+
+  webpack(config) {
+    config.module.rules.push(
+      {
+        test: /\.svg$/i,
+        issuer: /\.[jt]sx?$/,
+        resourceQuery: { not: [/url/] }, // exclude react component if *.svg?url
+        use: ['@svgr/webpack'],
+      },
+      {
+        test: /\.svg$/i,
+        type: 'asset',
+        resourceQuery: /url/,
+      },
+    )
+    return config
+  },
 }
 
 module.exports = nextConfig
