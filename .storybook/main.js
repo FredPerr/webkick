61c84a839b2f1f7d4893eee1ed9cbeccce0e3713
Frédéric Perron <frederic.norrep@gmail.com>
2023-01-10 21:38:35 -0500

    added storybook with a default story

---
+++ .storybook/main.js
@@ -0,0 +1,23 @@
+module.exports = {
+  stories: ['../components/**/*.stories.@(js|jsx|ts|tsx)'],
+  staticDirs: ['../public'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    '@storybook/addon-a11y',
+    '@storybook/addon-actions',
+    {
+      name: '@storybook/addon-storysource',
+      options: {
+        loaderOptions: {
+          injectStoryParameters: false,
+        },
+      },
+    },
+  ],
+  framework: '@storybook/react',
+  core: {
+    builder: '@storybook/builder-webpack5',
+  },
+}
---
+++ .storybook/preview.js
@@ -0,0 +1,19 @@
+import '/styles/globals.css'
+
+import React from 'react'
+
+import { ThemeProvider } from 'styled-components'
+
+export const decorators = [
+  (Story) => <ThemeProvider theme={{}}>{Story()}</ThemeProvider>,
+]
+
+export const parameters = {
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+}
