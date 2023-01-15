963a37b1fe0da1469aaf0d8df3bf03cc671ce51a
Frédéric Perron <frederic.norrep@gmail.com>
2023-01-15 14:23:03 -0500

    added babel config

--- components/box/Box.tsx
+++ components/box/Box.tsx
@@ -1,12 +1,12 @@
-import styled from "styled-components"
-import { CssProps } from "@/components"
+import styled from 'styled-components'
+import { CssProps } from '@/components'
 
 export interface BoxProps {
-    children?: React.ReactNode
+  children?: React.ReactNode
 }
 
 const Box = styled.div<BoxProps>`
-    color: blue;
+  color: blue;
 `
 
 export default Box
