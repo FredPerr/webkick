e62efbb04adfb9096a9892c69ee84f9867551ae8
Frédéric Perron <frederic.norrep@gmail.com>
2023-01-15 00:24:53 -0500

    put types into single file

--- components/index.ts
+++ components/index.ts
@@ -3,21 +3,3 @@ import { Color } from '@/styles/theme/colors'
 
 export { default as Button } from './button'
 export { default as Box } from './box'
-
-export interface StyledComponentProps {
-  css?: CSSObject
-}
-
-export interface VariantComponentProps {
-  color: Color
-  variant: string
-}
-
-export interface HoverComponentProps {
-  hoverFgColor: Color
-  hoverBgColor: Color
-}
-
-export interface StyledContainerProps {
-  containerCss?: CSSObject
-}
---
+++ components/ui.d.ts
@@ -0,0 +1,17 @@
+export interface StyledComponentProps {
+  css?: CSSObject
+}
+
+export interface VariantComponentProps {
+  color: Color
+  variant: string
+}
+
+export interface HoverComponentProps {
+  hoverFgColor: Color
+  hoverBgColor: Color
+}
+
+export interface StyledContainerProps {
+  containerCss?: CSSObject
+}
