ae50538e7473875bbd2dd7494c707b939e1c57e0
Frédéric Perron <frederic.norrep@gmail.com>
2023-01-15 10:12:12 -0500

    fixed import error

--- components/box/Box.tsx
+++ components/box/Box.tsx
@@ -1,11 +1,11 @@
+import type { StyledComponentProps } from '@/styles/theming'
 import styled from 'styled-components'
-import { StyledComponentProps } from '@/components'
 
 interface BoxProps extends StyledComponentProps {
   children?: React.ReactNode
 }
 
-const StyledBox = styled.div.attrs()<BoxProps>`
+const StyledBox = styled.div<BoxProps>`
   ${(props) => props.css}
 `
 
