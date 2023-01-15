7c30a3061da723924fb585130aedef24ad3af445
Frédéric Perron <frederic.norrep@gmail.com>
2023-01-15 14:32:00 -0500

    fixed css prop lint error

--- types/styled.d.ts
+++ types/styled.d.ts
@@ -1,4 +1,5 @@
 import type { ThemeType } from '@/styles/theme'
+import {} from 'styled-components/cssprop'
 
 declare module 'styled-components' {
   export interface DefaultTheme extends ThemeType {}
