d4ce1bac77e6f11fd4326a3adcb24642c7eb71e4
Frédéric Perron <frederic.norrep@gmail.com>
2023-01-15 21:15:13 -0500

    added the button component

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,13 +2,18 @@ import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import { DefaultLayout } from 'layouts'
 import Button from '@/components/button'
 
-
 export default function Home() {
   return (
     <>
-    <Button variant='contained' color='primary' arrow shadow>Obtenir ma soumission</Button>
-    <Button variant='outlined' color='primary' arrow>Contact us</Button>
-    <Button variant='text' color='primary' arrow>Contact us</Button>
+      <Button variant="contained" color="primary" arrow shadow>
+        Obtenir ma soumission
+      </Button>
+      <Button variant="outlined" color="primary" arrow>
+        Contact us
+      </Button>
+      <Button variant="text" color="primary" arrow>
+        Contact us
+      </Button>
     </>
   )
 }
