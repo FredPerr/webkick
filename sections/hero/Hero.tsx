65dcf4d83456cf8d577c066e0b08fbcc0c0886cd
Frédéric Perron <frederic.norrep@gmail.com>
2023-01-18 11:09:40 -0500

    removed unused imports

--- sections/hero/Hero.tsx
+++ sections/hero/Hero.tsx
@@ -1,6 +1,5 @@
 import Box from '@/components/box'
 import { useTheme } from 'styled-components'
-import styles from './Hero.module.css'
 import HeroBackgroundAnimation from './HeroBackgroundCanvas'
 
 export default function Hero() {
@@ -9,9 +8,8 @@ export default function Hero() {
     <Box
       css={{
         height: '100vh',
-        animation: `6s ${styles.gradient} infinite alternate`,
         backgroundSize: '400% 400%',
-        clipPath: 'polygon(0 0, 100% 0, 100% 30%, 0 80%)',
+        // clipPath: 'polygon(0 0, 100% 0, 100% 30%, 0 80%)',
       }}
     >
       <HeroBackgroundAnimation />
