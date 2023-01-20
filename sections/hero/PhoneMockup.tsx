944de69ed775cc860a355f26c27f7b03ebdb3091
Frédéric Perron <frederic.norrep@gmail.com>
2023-01-20 01:18:23 -0500

    added responsiveness to mockups

--- sections/hero/PhoneMockup.tsx
+++ sections/hero/PhoneMockup.tsx
@@ -1,10 +1,15 @@
 import Box from '@/components/box'
+import { useTheme } from 'styled-components'
 
 export default function PhoneMockup(props: { children?: React.ReactNode }) {
+  const theme = useTheme()
   return (
     <Box
       center
       css={{
+        [theme.media.xs]: {
+          display: 'none',
+        },
         backgroundColor: '#f2f5f9',
         boxShadow:
           '0 40px 50px 12px rgba(0, 0, 0, 0.11), inset 0 0 5px 2px #d5d4d8',
@@ -12,7 +17,7 @@ export default function PhoneMockup(props: { children?: React.ReactNode }) {
         height: 500,
         borderRadius: 40,
         position: 'absolute',
-        top: '30px',
+        top: '40%',
         left: '15%',
         transform: 'translateY(-50%)',
       }}
--- sections/hero/ScreenMockup.tsx
+++ sections/hero/ScreenMockup.tsx
@@ -1,15 +1,22 @@
+import { useTheme } from 'styled-components'
+
 export default function ScreenMockup() {
+  const theme = useTheme()
   return (
     <div
       css={{
+        [theme.media.xs]: {
+          display: 'none',
+        },
         background:
           'linear-gradient(hsla(0,0%,100%,.4),hsla(0,0%,100%,.3) 25%,rgba(246,249,252,.3) 50%,#f6f9fc 60%)',
         width: 800,
         height: 500,
         borderRadius: 10,
         overflow: 'hidden',
+        top: '32%',
         position: 'absolute',
-        left: '35%',
+        left: '40%',
         transform: 'translateY(-50%)',
         boxShadow: '0 40px 50px 12px rgba(0, 0, 0, 0.11)',
       }}
