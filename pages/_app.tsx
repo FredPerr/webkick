0f1dad9bd4627ec644a89291b793f374640ed169
Frédéric Perron <frederic.norrep@gmail.com>
2023-02-15 22:45:36 -0500

    configured sass

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import '@/styles/globals.scss'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import '@/styles/app.scss'
 import type { AppProps } from 'next/app'
 import { appWithTranslation } from 'next-i18next'
 import { NextPageWithLayout } from 'layouts/Default'
--- sections/HomeHero.tsx
+++ sections/HomeHero.tsx
@@ -1,3 +1,5 @@
+import styles from './HomeHero.module.scss'
+
 export default function HomeHero() {
-  return <div className=""></div>
+  return <div className={styles.hero_section}></div>
 }
