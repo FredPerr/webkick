2bce5f02ed56c580d1afab4341e174ace835fc0f
Frédéric Perron <frederic.norrep@gmail.com>
2023-02-16 19:06:51 -0500

    added hero img and responsiveness

--- sections/HeroImage.tsx
+++ sections/HeroImage.tsx
@@ -10,12 +10,11 @@ interface FrameProps {
 const imgNames = ['decotia', 'carua']
 
 function HeroImage() {
-
   const [frame, setFrame] = React.useState(0)
 
   React.useEffect(() => {
     const interval = setInterval(() => {
-      setFrame((prev)=> ((prev + 1) % imgNames.length))
+      setFrame((prev) => (prev + 1) % imgNames.length)
     }, 8000)
     return () => clearInterval(interval)
   }, [])
@@ -23,24 +22,40 @@ function HeroImage() {
   return (
     <div className={styles.hero_img}>
       <LaptopFrame name={imgNames[frame]} visisble={frame == 0} />
-      <MobileFrame name={imgNames[frame]} visisble={frame == 1}/>
+      <MobileFrame name={imgNames[frame]} visisble={frame == 1} />
     </div>
   )
 }
 
 function LaptopFrame(props: FrameProps) {
   return (
-      <div className={styles.laptop_frame}>
-        <Image className={`${styles.laptop_frame_img} ${props.visisble && styles.current_frame}`} src={`/images/hero/${props.name}-laptop.jpg`} alt={props.name} width={2880} height={1600}/>
-      </div>
+    <div className={styles.laptop_frame}>
+      <Image
+        className={`${styles.laptop_frame_img} ${
+          props.visisble && styles.current_frame
+        }`}
+        src={`/images/hero/${props.name}-laptop.jpg`}
+        alt={props.name}
+        width={2880}
+        height={1600}
+      />
+    </div>
   )
 }
 
 function MobileFrame(props: FrameProps) {
   return (
-      <div className={styles.mobile_frame}>
-        <Image className={`${styles.mobile_frame_img} ${props.visisble && styles.current_frame}`} src={`/images/hero/${props.name}-mobile.jpg`} alt={props.name} width={780} height={1688}/>
-      </div>
+    <div className={styles.mobile_frame}>
+      <Image
+        className={`${styles.mobile_frame_img} ${
+          props.visisble && styles.current_frame
+        }`}
+        src={`/images/hero/${props.name}-mobile.jpg`}
+        alt={props.name}
+        width={780}
+        height={1688}
+      />
+    </div>
   )
 }
 
--- sections/HomeHero.tsx
+++ sections/HomeHero.tsx
@@ -6,7 +6,9 @@ export default function HomeHero() {
     <section className={styles.hero_section}>
       <div className={styles.hero_container}>
         <div className={styles.hero_text_container}>Hello, I'm John Doe</div>
-        <div className={styles.hero_img_container}><HeroImage/></div>
+        <div className={styles.hero_img_container}>
+          <HeroImage />
+        </div>
       </div>
     </section>
   )
