c8c43545feeebe640f2bae58cb97dd3278b776a6
Frédéric Perron <frederic.norrep@gmail.com>
2023-02-16 19:06:26 -0500

    added the hero img and responsiveness

--- sections/HeroImage.tsx
+++ sections/HeroImage.tsx
@@ -1,3 +1,47 @@
-export default function HeroImage() {
-  return <div className="hero_img"></div>
+import Image from 'next/image'
+import styles from './HeroImage.module.scss'
+import React from 'react'
+
+interface FrameProps {
+  name: string
+  visisble?: boolean
 }
+
+const imgNames = ['decotia', 'carua']
+
+function HeroImage() {
+
+  const [frame, setFrame] = React.useState(0)
+
+  React.useEffect(() => {
+    const interval = setInterval(() => {
+      setFrame((prev)=> ((prev + 1) % imgNames.length))
+    }, 8000)
+    return () => clearInterval(interval)
+  }, [])
+
+  return (
+    <div className={styles.hero_img}>
+      <LaptopFrame name={imgNames[frame]} visisble={frame == 0} />
+      <MobileFrame name={imgNames[frame]} visisble={frame == 1}/>
+    </div>
+  )
+}
+
+function LaptopFrame(props: FrameProps) {
+  return (
+      <div className={styles.laptop_frame}>
+        <Image className={`${styles.laptop_frame_img} ${props.visisble && styles.current_frame}`} src={`/images/hero/${props.name}-laptop.jpg`} alt={props.name} width={2880} height={1600}/>
+      </div>
+  )
+}
+
+function MobileFrame(props: FrameProps) {
+  return (
+      <div className={styles.mobile_frame}>
+        <Image className={`${styles.mobile_frame_img} ${props.visisble && styles.current_frame}`} src={`/images/hero/${props.name}-mobile.jpg`} alt={props.name} width={780} height={1688}/>
+      </div>
+  )
+}
+
+export default HeroImage
--- sections/HomeHero.tsx
+++ sections/HomeHero.tsx
@@ -1,12 +1,12 @@
-import Image from 'next/image'
 import styles from './HomeHero.module.scss'
+import HeroImage from './HeroImage'
 
 export default function HomeHero() {
   return (
     <section className={styles.hero_section}>
       <div className={styles.hero_container}>
         <div className={styles.hero_text_container}>Hello, I'm John Doe</div>
-        <div className={styles.hero_img_container}></div>
+        <div className={styles.hero_img_container}><HeroImage/></div>
       </div>
     </section>
   )
