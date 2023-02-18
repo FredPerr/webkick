fc56f31cab08f1b9c9e2847d353e3b0e5fe72c38
Frédéric Perron <frederic.norrep@gmail.com>
2023-02-18 14:18:00 -0500

    added solution section

--- sections/solution/SolutionCard.tsx
+++ sections/solution/SolutionCard.tsx
@@ -1,3 +1,15 @@
-export default function SolutionCard() {
-  return <div></div>
+import styles from './SolutionCard.module.scss'
+
+interface SolutionCardProps {
+    title: string;
+    subtitle: string;
+    img: React.ReactNode
+}
+
+export default function SolutionCard(props: SolutionCardProps) {
+  return <div className={styles.container}>
+    {props.img}
+    <h3 className={styles.title}>{props.title}</h3>
+    <p className={styles.subtitle}>{props.subtitle}</p>
+  </div>
 }
--- sections/solution/SolutionSection.tsx
+++ sections/solution/SolutionSection.tsx
@@ -1,5 +1,10 @@
 import SectionTitle from '@/components/section/SectionTitle'
 import styles from './SolutionSection.module.scss'
+import SolutionCard from './SolutionCard'
+import { Button } from 'react-bootstrap'
+import LightBulbIllustration from '@/components/illustrations/LightBulbIllustration'
+import ThunderstormIllustration from '@/components/illustrations/ThunderstormIllustration'
+import ConversationIllustration from '@/components/illustrations/ConversationIllustration'
 
 export default function SolutionSection() {
   return (
@@ -8,7 +13,30 @@ export default function SolutionSection() {
         title="Professionnel, fiable et transparent"
         subtitle="Une solution exhaustive pour vous offrir la paix d’esprit"
       />
-      <div></div>
+      <div className={styles.cards_container}>
+        <SolutionCard
+          title="1. Commandez votre site"
+          subtitle="Obtenez votre soumission en 24H-48H et commençons le processus de création immédiatement."
+          img={
+            <LightBulbIllustration/>
+          }
+        />
+        <SolutionCard
+          title="2. Laissez-nous alimenter votre site"
+          subtitle="Nous hébergeons votre site sur des serveurs fiables et nous gérons toutes les technicalités."
+          img={
+            <ThunderstormIllustration/>
+          }
+        />
+        <SolutionCard
+          title="3. Maintenance régulière & support illimité"
+          subtitle="Demandez-nous de modifer votre site gratuitement & dormez sur vos deux oreilles avec notre support continu."
+          img={
+            <ConversationIllustration/>
+          }
+        />
+      </div>
+      <Button variant="primary">En savoir plus</Button>
     </section>
   )
 }
