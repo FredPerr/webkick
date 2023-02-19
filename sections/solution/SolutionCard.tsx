57e0e66538dd366c7297005393eb151a1b8416d4
Frédéric Perron <frederic.norrep@gmail.com>
2023-02-18 19:40:29 -0500

    removed useless imports

--- sections/solution/SolutionCard.tsx
+++ sections/solution/SolutionCard.tsx
@@ -1,15 +1,17 @@
 import styles from './SolutionCard.module.scss'
 
 interface SolutionCardProps {
-    title: string;
-    subtitle: string;
-    img: React.ReactNode
+  title: string
+  subtitle: string
+  img: React.ReactNode
 }
 
 export default function SolutionCard(props: SolutionCardProps) {
-  return <div className={styles.container}>
-    {props.img}
-    <h3 className={styles.title}>{props.title}</h3>
-    <p className={styles.subtitle}>{props.subtitle}</p>
-  </div>
+  return (
+    <div className={styles.container}>
+      {props.img}
+      <h3 className={styles.title}>{props.title}</h3>
+      <p className={styles.subtitle}>{props.subtitle}</p>
+    </div>
+  )
 }
