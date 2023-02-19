e98724c894505f69529b7c73fc3dff332d304c6d
Frédéric Perron <frederic.norrep@gmail.com>
2023-02-19 12:17:42 -0500

    add react-bootstrap and fixed navbar

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -3,80 +3,65 @@ import styles from './Navbar.module.scss'
 import NextLink from 'next/link'
 import React from 'react'
 import useScrollPosition from '@/hooks/useScrollPosition'
+import BSNavbar from 'react-bootstrap/Navbar'
+import { Button, Nav } from 'react-bootstrap'
 
 export default function Navbar() {
   const [expanded, setExpanded] = React.useState(false)
   const scrollPos = useScrollPosition()
   return (
-    <nav
-      className={`navbar navbar-expand-lg ${styles.navbar} ${
-        scrollPos > 5 && styles.navbar_scrolled
-      }`}
+    <BSNavbar
+      expand="lg"
+      fixed="top"
+      className={`${styles.navbar} ${scrollPos > 5 && styles.navbar_scrolled}`}
+      onToggle={() => setExpanded(!expanded)}
     >
       <div className={styles.navbar_content}>
         <div className={styles.navbar_brand_container}>
-          <button
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
+          <BSNavbar.Toggle
+            aria-controls="navbar-collapse"
             aria-label="Toggle navigation"
-            className={`navbar-toggler ${styles.navbar_toggle} ${
+            className={`${styles.navbar_toggle} ${
               expanded ? styles.collapsed : ''
             }`}
           />
           <NextLink href="/#" legacyBehavior passHref>
-            <a className="navbar-brand" href="/#">
+            <BSNavbar.Brand>
               <NextImage
                 src="/images/logo.svg"
                 width={0.65 * 263}
                 height={0.65 * 42}
                 alt="webkick logo"
               />
-            </a>
+            </BSNavbar.Brand>
           </NextLink>
         </div>
-        <div className={styles.navbar_nav_container}>
-          <ul
-            className={`navbar-nav me-auto mb-2 mb-lg-0 ${styles.navbar_collapse}`}
-            id="toggle-navigation-pane"
-          >
-            <li className={`nav-item ${styles.navbar_nav}`}>
-              <NextLink href="/#solution" passHref legacyBehavior>
-                <a className="nav-link">Solution</a>
-              </NextLink>
-            </li>
-            <li className={`nav-item ${styles.navbar_nav}`}>
-              <NextLink href="/#process" passHref legacyBehavior>
-                <a className="nav-link">Processus</a>
-              </NextLink>
-            </li>
-            <li className={`nav-item ${styles.navbar_nav}`}>
-              <NextLink href="/#pricing" passHref legacyBehavior>
-                <a className="nav-link">Tarification</a>
-              </NextLink>
-            </li>
-            <li className={`nav-item ${styles.navbar_nav}`}>
-              <NextLink href="/blog" passHref legacyBehavior>
-                <a className="nav-link">Blogue</a>
-              </NextLink>
-            </li>
-            <li className={`nav-item ${styles.navbar_nav}`}>
-              <NextLink href="/contact" passHref legacyBehavior>
-                <a className="nav-link">Nous contacter</a>
-              </NextLink>
-            </li>
-            <li>
-              <NextLink href="/#quote">
-                <button type="button" className="btn btn-primary">
-                  Obtenir ma soumission
-                </button>
-              </NextLink>
-            </li>
-          </ul>
-        </div>
+        <BSNavbar.Collapse
+          id="navbar-collapse"
+          className={styles.navbar_collapse}
+        >
+          <Nav className={`${styles.navbar_nav}`} navbarScroll>
+            <NextLink href="/#solution" passHref legacyBehavior>
+              <Nav.Link>Solution</Nav.Link>
+            </NextLink>
+            <NextLink href="/#process" passHref legacyBehavior>
+              <Nav.Link>Processus</Nav.Link>
+            </NextLink>
+            <NextLink href="/#pricing" passHref legacyBehavior>
+              <Nav.Link>Tarification</Nav.Link>
+            </NextLink>
+            <NextLink href="/blog" passHref legacyBehavior>
+              <Nav.Link>Blogue</Nav.Link>
+            </NextLink>
+            <NextLink href="/contact" passHref legacyBehavior>
+              <Nav.Link>Nous contacter</Nav.Link>
+            </NextLink>
+            <NextLink href="/#quote" passHref legacyBehavior>
+              <Button>Obtenir ma soumission</Button>
+            </NextLink>
+          </Nav>
+        </BSNavbar.Collapse>
       </div>
-    </nav>
+    </BSNavbar>
   )
 }
