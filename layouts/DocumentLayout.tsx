534d0b1b0f2beb7aa6ae35562a8e1adaabb7fcb4
Frédéric Perron <frederic.norrep@gmail.com>
2023-03-19 17:25:51 -0400

    added the legal pages

--- layouts/DocumentLayout.tsx
+++ layouts/DocumentLayout.tsx
@@ -1,15 +1,21 @@
 import Layout, { LayoutProps } from './Default'
-import styles from './DocumentLayout.module.css'
+import styles from './DocumentLayout.module.scss'
 
 interface DocumentLayoutProps extends LayoutProps {
   title: string
+  small: React.ReactNode
 }
 
 export default function DocumentLayout(props: DocumentLayoutProps) {
   return (
     <Layout pageName={props.pageName}>
-      <h1 className={styles.title}>{props.title}</h1>
-      {props.children}
+      <main className={styles.container}>
+        <div className={styles.title_container}>
+          <h1 className={styles.title}>{props.title}</h1>
+          {props.small}
+        </div>
+        {props.children}
+      </main>
     </Layout>
   )
 }
