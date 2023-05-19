946d574a4dacb2cabd7b99d0def897c26ba7a901
Frédéric Perron <frederic.perr@gmail.com>
2023-05-18 23:02:41 -0400

    fix(seo): updated favicon for google requirements

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,29 +8,10 @@ export default function Document() {
           property="og:image"
           content="/images/favicons/favicon-32x32.png"
         />
-        <meta name="msapplication-TileColor" content="#FFFFFF" />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/images/favicons/apple-touch-icon.png"
-        />
         <link
           rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/images/favicons/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/images/favicons/favicon-16x16.png"
-        />
-        <link rel="manifest" href="/images/favicons/site.webmanifest" />
-        <link
-          rel="mask-icon"
-          href="/images/favicons/safari-pinned-tab.svg"
-          color="#40c75e"
+          sizes="48x48"
+          href="/favicon.ico"
         />
         <meta name="google" content="nositelinkssearchbox" />
         <meta name="google" content="notranslate" />
